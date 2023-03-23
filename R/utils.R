f23e03f2c1af4fb778c9cf15e67c933dac6213b8
Joe Marlo <joe.marlo.09@gmail.com>
2023-03-23 16:00:31 +0000

    added pkgdown site

---
+++ R/utils.R
@@ -0,0 +1,11 @@
+
+#' Test function
+#'
+#' @return called for side effect
+#' @export
+#'
+#' @examples
+#' hello_world()
+hello_world <- function(){
+  print("hello world")
+}
--- dev/dev.R
+++ dev/dev.R
@@ -8,15 +8,14 @@ usethis::git_vaccinate()
 # imports
 usethis::use_package("htmltools", min_version = '0.5.4')
 usethis::use_package("data.table", min_version = '1.14.6')
+usethis::use_data_table()
 usethis::use_package('dplyr', min_version = '1.1.0')
 usethis::use_package('tidyr', min_version = '1.3.0')
 usethis::use_package('lubridate', min_version = '1.9.1')
 usethis::use_package('glue', min_version = '1.6.2')
 usethis::use_package('cli', min_version = '3.6.0')
 usethis::use_package('testthat', min_version = '3.1.6', type = 'Suggests')
-
-# usethis::use_data_table()
-# usethis::use_github_action()
+usethis::use_data_table()
 
 # create function files
 usethis::use_r('utils')
@@ -25,9 +24,17 @@ usethis::use_r('utils')
 usethis::use_testthat()
 usethis::use_test('utils')
 
-# github pages
-
 # other
+# usethis::use_github_action()
+# usethis::use_mit_license()
+
+# pkgdown website
+usethis::use_pkgdown()
+# created gh-pages branch manually
+# created this file manually: .github/workflows/pkgdown.yaml
+# site site local using pkgdown::build_site(devel = FALSE)
+
+# testing
 # devtools::load_all()
 # devtools::document()
 
